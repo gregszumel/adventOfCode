0d9deb9ea9f3b8dde62702a015d913956a22f25c
Greg Szumel <greg.szumel@gmail.com>
2023-09-22 13:17:07 -0400

    q3 p2 go

--- 3/go/run.go
+++ 3/go/run.go
@@ -14,6 +14,10 @@ func getVal(num int) int {
     }
 }
 
+func bothContains(letter, sack2, sack3 string) bool {
+    return strings.Contains(sack2, letter) && strings.Contains(sack3, letter)
+}
+
 
 func main() {
     runningSum := 0
@@ -22,6 +26,8 @@ func main() {
         panic(err)
     }
     unsplitRucksacks := strings.Split(string(dat), "\n")
+
+    // Part 1
     for i:=0; i < len(unsplitRucksacks); i++ {
         targetRucksack := unsplitRucksacks[i]
         left := targetRucksack[:len(targetRucksack) / 2]
@@ -35,6 +41,21 @@ func main() {
         }
     }
 
-    fmt.Println(runningSum)
+    fmt.Println("part 1 solution:", runningSum)
 
+    // Part 2
+    part2Sum := 0
+    for i:=0; i < len(unsplitRucksacks) - 3; i = i + 3 {
+        sack1 := unsplitRucksacks[i]
+        sack2 := unsplitRucksacks[i+1]
+        sack3 := unsplitRucksacks[i+2]
+        for letterIdx:=0; letterIdx < len(sack1); letterIdx++ {
+            targetLetter := string(sack1[letterIdx])
+            if bothContains(targetLetter, sack2, sack3) {
+                part2Sum += getVal(int(sack1[letterIdx]))
+                break;
+            }
+        }
+    }
+    fmt.Println("part 2 solution:", part2Sum)
 }
