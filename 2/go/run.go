dd476797de7f7743370d6575cf422f4505aaf2e0
Greg Szumel <greg.szumel@gmail.com>
2023-09-21 20:24:51 -0400

    q2 p2 go

--- 2/go/run.go
+++ 2/go/run.go
@@ -25,11 +25,32 @@ func score(picks []string) int {
     return -1
 }
 
+func score2(picks []string) int{
+    player1, outcome := picks[0], picks[1]
+    switch {
+    case player1=="A" && outcome=="X": return 0 + 3
+    case player1=="B" && outcome=="X": return 0 + 1
+    case player1=="C" && outcome=="X": return 0 + 2
+
+    case player1=="A" && outcome=="Y": return 3 + 1
+    case player1=="B" && outcome=="Y": return 3 + 2
+    case player1=="C" && outcome=="Y": return 3 + 3
+
+    case player1=="A" && outcome=="Z": return 6 + 2
+    case player1=="B" && outcome=="Z": return 6 + 3
+    case player1=="C" && outcome=="Z": return 6 + 1
+    }
+
+    return -1
+}
+
+
 func main() {
     dat, err := os.ReadFile("input.txt")
     if err != nil { panic(err) }
     gamesStrings := strings.Split(string(dat), "\n")
     scoreSum := 0
+    part2ScoreSum := 0
 
     for i:=0; i < len(gamesStrings); i++ {
         gameStr := gamesStrings[i]
@@ -37,9 +58,12 @@ func main() {
             game := strings.Split(gamesStrings[i], " ")
             gameScore := score(game)
             scoreSum += gameScore
-            fmt.Println(game, "->", gameScore)
+            score2Sum := score2(game)
+            part2ScoreSum += score2Sum
+            fmt.Println(game, "->", gameScore, "||", score2Sum)
         }
     }
 
     fmt.Println(scoreSum)
+    fmt.Println(part2ScoreSum)
 }
